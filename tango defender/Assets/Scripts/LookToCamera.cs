ba8a5bb0c7d6621cec6ed13c263a173bb5b81746
Martin <hannanmartin@gmail.com>
2016-03-08 19:16:22 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	tango defender/Assets/Resources/Prefabs/Seekers.prefab
    #	tango defender/Assets/Scenes/game.unity
    #	tango defender/Library/CurrentLayout.dwlt
    #	tango defender/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll
    #	tango defender/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb
    #	tango defender/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	tango defender/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	tango defender/Library/assetDatabase3
    #	tango defender/Library/expandedItems
    #	tango defender/Library/metadata/00/00000000000000004000000000000000
    #	tango defender/Library/metadata/00/00000000000000006100000000000000
    #	tango
    defender/Library/metadata/07/070f99497758028428c2d9302c4648ca.info
    
    Health Bar, Camera Tracking, Health Scripts
    
    Added in the health bar
    system and included scripts that track the
    health bar to the Cameras
    direction.

---
+++ tango defender/Assets/Scripts/LookToCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookToCamera : MonoBehaviour {
+
+     GameObject cameran;
+
+
+    void Start()
+    {
+        cameran =GameObject.Find("Camera");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        transform.LookAt (transform.position + cameran.transform.rotation* Vector3.back,
+                                cameran.transform.rotation * Vector3.up);
+	}
+}
