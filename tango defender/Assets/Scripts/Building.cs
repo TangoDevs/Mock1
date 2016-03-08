16b9830fb3fc170a9d7c05b531d5cdc0a29165fe
mysterydeveloper <manus.duggan@yahoo.com>
2016-03-08 12:01:24 +0000

    Labels
    in building the labels are updating

--- tango defender/Assets/Scripts/Building.cs
+++ tango defender/Assets/Scripts/Building.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Building : MonoBehaviour {
 
@@ -7,8 +9,11 @@ public class Building : MonoBehaviour {
     GameObject prefab,plane,cam,border;
     Vector3 mousePosition, targetPosition,CheckPosition;	
    start s;
+	GameObject scoreLabelObject,goldLabelObject;
+	Text scoreTxt,goldTxt;
+	int score=0,gold=1000;
 
-    void Start()
+    void Awake()
     {
         building = false;
         prefab = (GameObject)Resources.Load("Prefabs/WoodTower");
@@ -16,36 +21,40 @@ public class Building : MonoBehaviour {
 		cam=GameObject.Find("camerarig");
 		s=cam.GetComponent<start>();
 		allowed=s.GetAllowed();
+		SetScore ();
+		SetGold ();
     }
 
     // Update is called once per frame
     void Update()
-    {     
-		allowed=s.GetAllowed();
+    {   
+		scoreTxt.text = " " + score;
+		goldTxt.text = " " + gold;
+		if (gold >= 100) {
+			allowed = s.GetAllowed ();
 
-		if(allowed==false){
-			//If Left Button is clicked
-			if (Input.GetMouseButtonDown(0))
-			{
-				//To get the current mouse position
-				mousePosition = Input.mousePosition;
-				//Convert the mousePosition according to World position
-				targetPosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 1));
+			if (allowed == false) {
+				//If Left Button is clicked
+				if (Input.GetMouseButtonDown (0)) {
+					//To get the current mouse position
+					mousePosition = Input.mousePosition;
+					//Convert the mousePosition according to World position
+					targetPosition = Camera.main.ScreenToWorldPoint (new Vector3 (mousePosition.x, mousePosition.y, 1));
 
 
-				//Set the position of targetObject
-				prefab.transform.position = targetPosition;
+					//Set the position of targetObject
+					prefab.transform.position = targetPosition;
 
-				targetPosition.y = plane.transform.position.y;
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit rch;
-				if (Physics.Raycast(ray, out rch))
-				{
-					if (rch.transform.name == "Plane")
-					{                   
-						building = true;
-						//create the instance of targetObject and place it at given position.
-						Instantiate(prefab, targetPosition, prefab.transform.rotation);
+					targetPosition.y = plane.transform.position.y;
+					Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+					RaycastHit rch;
+					if (Physics.Raycast (ray, out rch)) {
+						if (rch.transform.name == "Plane") {                   
+							building = true;
+							//create the instance of targetObject and place it at given position.
+							Instantiate (prefab, targetPosition, prefab.transform.rotation);
+							gold-=100;
+						}
 					}
 				}
 			}
@@ -63,5 +72,22 @@ public class Building : MonoBehaviour {
         building = newbuild;
     }
 
+	public void AddScore(int addScore){
+		score += addScore;
+	}
+
+	public void AddGold(int addGold){
+		gold += addGold;
+	}
+
+	void SetGold (){
+		goldLabelObject = GameObject.Find ("GoldCurrent");
+		goldTxt = goldLabelObject.GetComponent<Text>();
+	}
+
+	void SetScore (){
+		scoreLabelObject = GameObject.Find ("ScoreCurrent");
+		scoreTxt = scoreLabelObject.GetComponent<Text>();
+	}
   
 }
--- tango defender/Assets/Scripts/killer.cs
+++ tango defender/Assets/Scripts/killer.cs
@@ -2,23 +2,29 @@
 using System.Collections;
 
 public class killer : MonoBehaviour {
-
+	GameObject plane;
+	Building b;
 	// Use this for initialization
 	void Start () {
-	
+		plane=GameObject.Find("Plane");
+		b=plane.GetComponent<Building>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
     void OnTriggerEnter(Collider col)
     {
 
         {
-           // Destroy(col.gameObject);
+           //Destroy(col.gameObject);
+			b.AddGold(100);
+			b.AddScore (200);
+
         }
+
 
     }
 }
