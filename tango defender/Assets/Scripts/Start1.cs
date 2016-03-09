b12f99e2d116abfbce0bd30f5198fb9e510c2532
Martin <hannanmartin@gmail.com>
2016-03-09 02:25:15 +0000

    Added Start Menu

---
+++ tango defender/Assets/Scripts/Start1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Start1 : MonoBehaviour {
+
+    public void LoadStage()
+    {
+        SceneManager.LoadScene("game");
+    }
+}
